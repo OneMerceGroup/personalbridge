ece5042d2f534d229eb96c900518be82f2facc07
TruongTuyen <hieudieuluong@gmail.com>
2021-09-24 08:01:02 +0700

    Separate class

---
+++ inc/theme-normal.php
@@ -0,0 +1,21 @@
+<?php
+class PersonalBridge_Theme_Normal extends PersonalBridge_Theme_Base {
+	protected static $_instance = null;
+
+	public static function get_instance() {
+		if ( is_null( self::$_instance ) ) {
+			self::$_instance = new self();
+		}
+		return self::$_instance;
+	}
+
+	public function __construct() {
+
+	}
+
+}
+
+function personalbridge_theme_normal() {
+	return PersonalBridge_Theme_Normal::get_instance();
+}
+
